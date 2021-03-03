a6e53c81a7cb0c7f2c9e347333d675641beed4c5
Mətin Vüqaroğlu <metin.huseynzade525@gmail.com>
2021-03-03 18:33:19 +0000

    update

--- matrix input checker.py
+++ matrix input checker.py
@@ -2,8 +2,6 @@
 # https://github.com/chillmetin/iztech
 # https://github.com/faxrij/
 
-
-
 #1. Row Vector    ex: ( 1, 2, 3)
 
 #2. Column Vector ex: [ 1, 2, 3]
@@ -12,8 +10,7 @@
 #  [ ( 1, 2, 3), ( 4, 5, 6) ]
 #  ( [ 1, 4], [ 2, 5], [ 3, 6] )
 
-
-#4. Augmented Matrix ex: 
+#4. Augmented Matrix ex:
 #   [ ( 1, 1, 0| 2), ( -2, 0, 1| 0) ]
 #   ( [ 1, 4], [ 2, 5]| [ 3, 6] )
 
@@ -27,14 +24,14 @@ def row_vector(user_input):
     else:
         result = False
         return False
-    
 
     for char in liste:
         if not char[0] == ' ' or char[-1] == ' ' or char.count(" ") > 1:
             result = False
 
     return result
-        
+
+
 def column_vector(user_input):
 
     result = True
@@ -45,53 +42,57 @@ def column_vector(user_input):
     else:
         result = False
         return False
-    
 
     for i in splitted_list:
-        if i[0] != ' ' or i[-1]==' ' or i.count(" ") > 1:
+        if i[0] != ' ' or i[-1] == ' ' or i.count(" ") > 1:
             result = False
 
     return result
 
-def matrix(user_input):
+
+def matrix(input):
     result = True
-    for k in user_input:
-        if k.count(" ") > 1:
-            result = False
-            return result
-    paranthesis_check = (user_input.count('[') == user_input.count(']') and user_input.count('(') == user_input.count(')'))
-    if user_input[-2]!=' ':
-        result = False
-        return result
+    if input[:3] == "[ (" and input[-3:] == ") ]":  #row
+        raw_input = user_input[2:-2]
+        row_list = raw_input.split("), ")
+        index = 0
+        for entry in row_list:
+            if entry[-1] != ")":
+                row_list[index] += ")"
+
+        entries_per_row = []
+        for row in row_list:
+            if result != False: result = row_vector(row)
+
+            if entries_per_row != []:
+                if entries_per_row[-1] != row.count(',') + 1:
+                    return False
+            entries_per_row.append(row.count(',') + 1)
+            index += 1
+
+    elif input[:3] == '( [' and input[-3:] == "] )":  #column
+        result = True
+        raw_input = user_input[2:-2]
+        column_list = raw_input.split("], ")
+        index = 0
+        for entry in column_list:
+            if entry[-1] != "]":
+                column_list[index] += "]"
+            index += 1
+        entries_per_column = []
+        for column in column_list:
+            if result != False: result = column_vector(column)
+
+            if entries_per_column != []:
+                if entries_per_column[-1] != column.count(',') + 1:
+                    return False
+            entries_per_column.append(column.count(',') + 1)
+
     else:
-        if paranthesis_check:
-            
-            if user_input[:3]=='[ (':
-                new_List = user_input.strip('[ ').strip(' ]')
-                new_list2 = new_List.split('), ')
-                
-                for i in new_list2:
-                    if i[-1]!=')':
-                        i+=')'
-                    result = row_vector(i)
-                    if result==False:
-                        return result
-                return result
+        result = False
 
+    return result
 
-            elif user_input[:3]=='( [':
-                new_List = user_input.strip('( ').strip(' )')
-                new_list2 = new_List.split('], ')
-               
-                for i in new_list2:
-                    
-                    if i[-1]!=']':
-                        i+=']'
-                        
-                    result = column_vector(i)
-                    if result==False:
-                        return result
-                return result
 
 def augmented_matrix(input):
     result = True
@@ -158,34 +159,27 @@ def augmented_matrix(input):
         result = False
 
     return result
-    
-
 
 
 condition = True
 while condition:
-    
+
     user_input = input("check:\n\n")
-    
+
     if "|" in user_input:
-        input_type = 4        # Augmented Matrixp
-        print('\n'+str(augmented_matrix(user_input)))
+        input_type = 4  # Augmented Matrixp
+        print('\n' + str(augmented_matrix(user_input)))
 
-
-
-    elif user_input[:3]=='( [' or user_input[:3] == '[ (':
-        input_type = 3        # Matrix 
-        print('\n'+str(matrix(user_input)))
-        
+    elif user_input[:3] == '( [' or user_input[:3] == '[ (':
+        input_type = 3  # Matrix
+        print('\n' + str(matrix(user_input)))
 
     elif user_input[0] == '[':
-        input_type = 2        # Column Vector
-        print('\n'+str(column_vector(user_input)))
-
+        input_type = 2  # Column Vector
+        print('\n' + str(column_vector(user_input)))
 
     elif user_input[0] == '(':
-        input_type = 1        # Row Vector
-        print('\n'+str(row_vector(user_input)))
-        
+        input_type = 1  # Row Vector
+        print('\n' + str(row_vector(user_input)))
 
     print(50 * '-' + '\n')
